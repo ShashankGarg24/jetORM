a02e3c83c828654e7a74ffc699b490508516dc0f
Shashank Garg <shashank.garg@shl.com>
2023-08-22 17:05:58 +0530

    added column, entity and id annotations

---
+++ src/main/java/org/jetORM/annotations/Column.java
@@ -0,0 +1,15 @@
+package org.jetORM.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface Column {
+	
+	String name();
+	boolean nullable() default true;
+	boolean unique() default false;
+}
